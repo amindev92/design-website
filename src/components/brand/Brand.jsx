5aefcb1d3f3da32d8f71abc6cf36715659894d17
Amin Ataei <amin.ataei92@gmail.com>
2023-05-12 13:44:24 +0430

    complete brand section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './brand.css'
+import { Aws, Nodejs, Reactjs, Nextjs, Mysql } from './imports'
 
 const Brand = () => {
     return (
-        <div>Brand</div>
+        <div className='our-technology section--padding'>
+            <div className='our-technology__item'>
+                <img src={Aws} alt="Aws"></img>
+            </div>
+            <div className='our-technology__item'>
+                <img src={Nodejs} alt="Nodejs"></img>
+            </div>
+            <div className='our-technology__item'>
+                <img src={Reactjs} alt="Reactjs"></img>
+            </div>
+            <div className='our-technology__item'>
+                <img src={Nextjs} alt="Nextjs"></img>
+            </div>
+            <div className='our-technology__item'>
+                <img src={Mysql} alt="Mysql"></img>
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import Aws from '../../assets/images/png/aws.png'
+import Mysql from '../../assets/images/png/mysql.png'
+import Nextjs from '../../assets/images/png/nextjs.png'
+import Reactjs from '../../assets/images/png/react.png'
+import Nodejs from '../../assets/images/png/nodejs.png'
+
+export {
+    Aws,
+    Mysql,
+    Nextjs,
+    Reactjs,
+    Nodejs
+}
