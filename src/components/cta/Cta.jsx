ff45e2b2b6e6b4ddde67354b8b856dc2da5e8f4e
Amin Ataei <amin.ataei92@gmail.com>
2023-05-13 13:59:45 +0430

    complete cta section

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -4,7 +4,15 @@ import './cta.css'
 const Cta = () => {
     return (
 
-        <div>Cta</div>
+        <div className='cta section--margin'>
+            <div className='cta__title'>
+                <p>Register today and order your project.</p>
+                <h4>Register today and order your project for improve your job's productivity</h4>
+            </div>
+            <div className='cta__button'>
+                <button type='button'>Register</button>
+            </div>
+        </div>
 
     )
 }
