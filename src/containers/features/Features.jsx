dc1f92bc9c77216332f0a8727192026203f7a91e
Amin Ataei <amin.ataei92@gmail.com>
2023-05-13 13:47:21 +0430

    complete features and services section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Feature } from '../../components'
 import './features.css'
+
+const featuresData = [
+    {
+        title: 'Web App Development',
+        text: 'Web App Development we provides expert-level Web Development Services to turn your idea into a high-quality web-based solution – exactly the way you envisioned.'
+    },
+    {
+        title: 'Cloud Web',
+        text: 'With us, a trusted offshore web development vendor, you can implement complex cloud app development projects to succeed in a data-driven world.'
+    },
+    {
+        title: 'Web Optimization',
+        text: 'our experts will perform an audit of your web-based solutions and apps, with a range of A/B tests, generating a plan of improvements to undertake.'
+    },
+    {
+        title: 'Web Design',
+        text: 'Web Design If you need a web design company to deliver visually engaging IT projects with outstanding UI/UX, with its user-centered approach, is the best choice.'
+    }
+]
 
 const Features = () => {
     return (
-        <div>Features</div>
+        <div className='features section--padding'>
+            <div className='features__heading'>
+                <h2 className='gradiant--text'>Our Services</h2>
+                <p>Get to know our services</p>
+            </div>
+            <div className='features__container'>
+                {featuresData.map((item, index) => {
+                    return <Feature title={item.title} text={item.text} key={item.title + index} />
+                })}
+            </div>
+        </div>
     )
 }
 
--- src/containers/services/Services.jsx
+++ src/containers/services/Services.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './services.css'
+import imageServices from '../../assets/images/png/woman1.png'
 
 const Services = () => {
     return (
-        <div>Services</div>
+        <div className='services section--padding'>
+            <div className='services__image'>
+                <img src={imageServices} alt='Services'></img>
+            </div>
+            <div className='services__content'>
+
+                <span>Get to know our services</span>
+                <h3 className='gradiant--text'>Website and logo design</h3>
+                <p>
+                    Throughout all web development services, We adhere to a quality-first approach based on ISO 9001-certified quality management system. Our comprehensive security management program with robust security controls, policies and technologies is compliant with ISO 27001 regulations. </p>
+            </div>
+        </div>
     )
 }
 
